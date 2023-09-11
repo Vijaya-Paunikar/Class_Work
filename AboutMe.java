8e87a3f7d64d340b4cb0d661ad748fafbf5193ec
Vijaya-Paunikar <144651081+Vijaya-Paunikar@users.noreply.github.com>
2023-09-11 10:56:07 +0530

    Add files via upload

---
+++ AboutMe.java
@@ -0,0 +1,15 @@
+// program for printing AboutMe
+class AboutMe
+{
+   public static void main(String args[])
+   {
+     System.out.println("My name is Vijaya Paunikar");
+	 System.out.println("I am from Nagpur.");
+	 System.out.println("I recently complete my graduation in Computer Science Branch from KDKCE");
+	 System.out.println("My area of interest is Full Stack Development");
+	 System.out.println("I have knowledge in C, C++, Core Java, Frontent Development");
+	 System.out.println("My goal is to achieve a good position in reputed company where I can enhance my skilss and knowledge.");
+	 System.out.println("I am self-learner and self motivated kind of person");
+	 System.out.println("That's all about me.");
+   } 
+}
