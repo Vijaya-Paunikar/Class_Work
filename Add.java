1faab1875e0fd432f35325a33f7c7f627e2e37e2
Vijaya-Paunikar <144651081+Vijaya-Paunikar@users.noreply.github.com>
2023-09-11 10:57:44 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,13 @@
+// WAP for addition of two numbers.
+
+class Add
+{
+  public static void main(String args[])
+  {
+    int num1=34,num2=67,sum;  // variable declaration
+	sum=num1+num2;
+	System.out.println("The addition is "+sum);
+	System.out.println("The addition of "+num1+" and "+num2+" is "+sum);
+	System.out.println("The addition of"+" "+num1+" "+"and"+" "+num2+" "+"is"+" "+sum);
+  }
+}
